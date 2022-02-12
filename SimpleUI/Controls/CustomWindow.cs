1cedd2aaeabbbce0f3fdaa4055f0b142bae4b5d4
Greg <Greg@hackingadventures.ca>
2022-02-12 08:59:11 -0700

    Add project files.

---
+++ SimpleUI/Controls/CustomWindow.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace SimpleUI.Controls
+{
+    public class CustomWindow : Window
+    {
+        #region Click events
+        protected void MinimizeClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        protected void RestoreClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = (WindowState == WindowState.Normal) ? WindowState.Maximized : WindowState.Normal;
+        }
+
+        protected void CloseClick(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+        #endregion
+
+        static CustomWindow()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomWindow), new FrameworkPropertyMetadata(typeof(CustomWindow)));
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+
+            // Begin dragging the window
+            this.DragMove();
+        }
+
+        public override void OnApplyTemplate()
+        {
+            Button? minimizeButton = GetTemplateChild("minimizeButton") as Button;
+            if (minimizeButton != null)
+                minimizeButton.Click += MinimizeClick;
+
+            Button? restoreButton = GetTemplateChild("restoreButton") as Button;
+            if (restoreButton != null)
+                restoreButton.Click += RestoreClick;
+
+            Button? closeButton = GetTemplateChild("closeButton") as Button;
+            if (closeButton != null)
+                closeButton.Click += CloseClick;
+
+            base.OnApplyTemplate();
+        }
+    }
+}
